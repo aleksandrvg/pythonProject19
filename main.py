6c1e86aa81838e7dd1e358426daeb048e6c5f684
aleksandrvg <aleksandrvg79@gmail.com>
2024-02-13 20:33:29 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request
+app = Flask("flaskapp")
+
+@app.route("/")
+def HelloFlask():
+    return "Hello Flask"
+
+@app.route("/about")
+def About():
+    return render_template('about.html')
+
+@app.route("/watch", methods=['GET'])
+def Watch():
+    return render_template('watch.html')
+
+@app.route("/submit", methods=['POST'])
+def Submit():
+    username = request.form["username"]
+    return render_template("submit.html", username=username)
+
+if __name__ == "__main__":
+    app.run(debug=True)
